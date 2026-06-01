4001f18ab8c5110fa306cb056c6db28c1d8dcf8a
MachineryZ <zhizuozhang@gmail.com>
2026-06-01 13:11:03 +0800

    update

--- run_dashboard.sh
+++ run_dashboard.sh
@@ -3,3 +3,8 @@
 /opt/anaconda3/bin/streamlit run if_all.py --server.headless true --browser.gatherUsageStats false --server.port 1025 &
 /opt/anaconda3/bin/streamlit run cf_position.py --server.headless true --browser.gatherUsageStats false --server.port 2001 &
 /opt/anaconda3/bin/streamlit run if_position.py --server.headless true --browser.gatherUsageStats false --server.port 2002 &
+
+
+/opt/anaconda3/bin/streamlit run cncf_overall.py --server.headless true --browser.gatherUsageStats false --server.port 2222
+
+
